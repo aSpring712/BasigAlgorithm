e315fc817c69af93b3befcd6604049a2d650bdb0
BOMI KWON <80755461+aSpring712@users.noreply.github.com>
2021-04-30 21:42:17 +0900

    1039, 1038

---
+++ CodeUp100/codeUp/q1038.java
@@ -0,0 +1,13 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class q1038 {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        System.out.println(a + b);
+	}
+}
---
+++ CodeUp100/codeUp/q1039.java
@@ -0,0 +1,14 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class q1039 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().split(" ");
+        long a = Long.parseLong(str[0]);
+        long b = Long.parseLong(str[1]);
+        System.out.println(a + b);
+	}
+}
